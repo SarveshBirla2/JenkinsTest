af1f6c09ab6fbed3f338c49eaba1d3d1284f6fad
unknown <sarvesh.birla@wissen.com>
2025-02-13 16:16:58 +0530

    Test Commit

--- src/main/java/com/CalculatorMain.java
+++ src/main/java/com/CalculatorMain.java
@@ -9,6 +9,9 @@ public class CalculatorMain
 		Calculator c1 = new Calculator();
 		System.out.println("Addition : "+c1.add(10, 20));
 		System.out.println("Substraction : "+c1.sub(10, 20));
+                
+                System.out.println("Test change");
+
 	
 	}
 }
