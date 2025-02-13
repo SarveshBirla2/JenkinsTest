d47a6764513283a32e9f82b83288651c9165fcae
unknown <sarvesh.birla@wissen.com>
2025-02-13 15:47:31 +0530

    First Commit

---
+++ src/main/java/com/CalculatorMain.java
@@ -0,0 +1,14 @@
+package com;
+import com.Calculator;
+public class CalculatorMain
+{
+	public static void main(String args[])
+	{
+		System.out.println("Calculator Operations......");
+ 
+		Calculator c1 = new Calculator();
+		c1.add(10, 20);
+		c1.sub(10, 20);
+	
+	}
+}
